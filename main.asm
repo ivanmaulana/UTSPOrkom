8cd84c752d1c66bc61192db3eebf406b7078bbf2
IVAN MAULANA PUTRA <ivanmaulana@student.ipb.ac.id>
2016-04-09 02:39:06 +0700

    main.asm

---
+++ main.asm
@@ -0,0 +1,21 @@
+; @author   : Ivan Maulana
+; @nim  	: G64130076
+; @no		: 1. matrix determinant
+
+SECTION .data
+
+
+SECTION .bss
+   
+
+SECTION .text
+    global main
+
+main:
+
+
+exit:
+    ; exit program
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
